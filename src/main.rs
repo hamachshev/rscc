eb4c94a9537d0edbee0e1a4af3ce3df2c76bc6b8
Aharon Seidman <aharondov1@gmail.com>
2026-05-05 22:47:55 -0400

    added output file option

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,33 @@
 use rscc::{codegen, lexer, parser};
-use std::{env::args, path::PathBuf};
+use std::{env::args, ffi::OsString, os::unix::ffi::OsStrExt, path::PathBuf};
 
 #[allow(unused)]
 fn main() {
-    let path: PathBuf = args()
-        .nth(1)
-        .expect("Must input a c file to compile")
-        .into();
+    let mut args = args();
+    let mut path: PathBuf = args.nth(1).expect("Must input a c file to compile").into();
+    let output_filename = match args.next() {
+        Some(arg) if arg == "-o" => Some(args.next().expect("Expected filename after -o")),
+        Some(_) => panic!("Unexpected argument. Usage <input.c> [-o output]"),
+        None => None,
+    };
 
-    let file = std::fs::File::open(path).unwrap();
+    let file = std::fs::File::open(&path).unwrap();
     let lex = lexer::lex(file).unwrap();
     let parse = parser::parse_program(&mut lex.into_iter());
-    println!("{}", parse);
-    println!("{}", codegen::gen_program(parse));
+    let codegen = codegen::gen_program(parse);
+    let output_filename = match output_filename {
+        Some(n) => OsString::from(n),
+        None => {
+            path.set_extension("o");
+            OsString::from(path)
+        }
+    };
+    let output_filename_string = String::from_utf8_lossy(output_filename.as_bytes());
+    std::fs::write(&output_filename, codegen).expect(&format!(
+        "couldnt write to file {}",
+        &output_filename_string
+    ));
+    println!("compiled to {}", output_filename_string)
 }
 
 #[cfg(test)]
